94217aacca4c09721f486b7cf87e70b72fbe3e02
Erica Hashert <ericahashert@gmail.com>
2022-09-23 12:14:37 -0600

    commit tree 913c2a4911d4cd08ba58a640728ad551ce6c9a23

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,108 @@
+class TicTacToe
+
+    attr_accessor :board
+
+    WIN_COMBINATIONS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8], 
+        [2, 4, 6],
+    ]
+
+    def initialize 
+        @board = [" ", " "," "," "," "," "," "," "," "]
+    end
+
+    def display_board ()
+        puts " #{board[0]} | #{board[1]} | #{board[2]} "
+        puts "-----------"
+        puts " #{board[3]} | #{board[4]} | #{board[5]} "
+        puts "-----------"
+        puts " #{board[6]} | #{board[7]} | #{board[8]} "
+    end
+
+    def input_to_index (user_input)
+        user_input.to_i - 1
+    end
+
+    def move (index, players_token = x)
+        board[index] = players_token  
+    end
+
+    def position_taken? (index)
+        if board[index] == " "
+            false
+        else
+            true
+        end
+    end
+
+    def valid_move? (index)
+            if !position_taken?(index) && index < 9
+                true
+            else
+                false
+            end
+    end
+
+    def turn_count
+        counter = 0
+        board.each do |input|
+            if input == 'X' || input == 'O'
+                counter += 1
+            end
+        end
+        counter
+    end
+
+    def current_player
+        turn_count.even? ? 'X' : 'O'
+    end
+
+    def turn
+        user_input = gets.strip
+        index = input_to_index(user_input)
+        if valid_move?(index)
+            token = current_player
+            move(index, token)
+        else
+            turn
+        end
+        display_board
+    end
+    
+
+    def won?
+        WIN_COMBINATIONS.any? do |combo|
+            if position_taken?(combo[0]) && @board[combo[0]] == @boarad[combo[1]] && @board[combo[1]] == @board[combo[2]]
+                return combo
+            end
+        end
+    end
+
+    def full?
+        
+    end
+
+    def draw?
+        
+    end
+
+    def over?
+        
+    end
+
+    def winner
+        
+    end
+
+    def play
+        
+    end
+
+
+end
